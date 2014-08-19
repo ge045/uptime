77409e87af37e30bb5dd8f57bc959b56552cbbfe
georg <georg.ogris@gmail.com>
2014-08-19 16:43:34 +0200

    added a "-a" command line switch for
    added a "-a" command line switch for displaying both uptime and boottime

--- src/__main__.py
+++ src/__main__.py
@@ -10,6 +10,29 @@ try:
 except Exception:
     pass
 
+def _uptime(up):
+    parts = []
+
+    days, up = up // 86400, up % 86400
+    if days:
+        parts.append('%d day%s' % (days, 's' if days != 1 else ''))
+
+    hours, up = up // 3600, up % 3600
+    if hours:
+        parts.append('%d hour%s' % (hours, 's' if hours != 1 else ''))
+
+    minutes, up = up // 60, up % 60
+    if minutes:
+        parts.append('%d minute%s' % (minutes, 's' if minutes != 1 else ''))
+
+    if up or not parts:
+        parts.append('%.2f seconds' % up)
+
+    sys.stdout.write('Uptime: %s.\n' % ', '.join(parts))
+    
+def _boottime():
+    sys.stdout.write(boottime().strftime('Booted: %c.\n'))
+    
 if __name__ == '__main__':
     up = uptime()
 
@@ -17,24 +40,10 @@ if __name__ == '__main__':
         sys.stderr.write('Unable to determine uptime. Patches welcome.\n')
         sys.exit(1)
 
-    if '-b' not in sys.argv:
-        parts = []
-
-        days, up = up // 86400, up % 86400
-        if days:
-            parts.append('%d day%s' % (days, 's' if days != 1 else ''))
-
-        hours, up = up // 3600, up % 3600
-        if hours:
-            parts.append('%d hour%s' % (hours, 's' if hours != 1 else ''))
-
-        minutes, up = up // 60, up % 60
-        if minutes:
-            parts.append('%d minute%s' % (minutes, 's' if minutes != 1 else ''))
-
-        if up or not parts:
-            parts.append('%.2f seconds' % up)
-
-        sys.stdout.write('Uptime: %s.\n' % ', '.join(parts))
+    if '-a' in sys.argv:
+        _uptime(up)
+        _boottime()
+    elif '-b' not in sys.argv:
+        _uptime(up)
     else:
-        sys.stdout.write(boottime().strftime('Booted: %c.\n'))
+        _boottime()
